cc113cb4d6d6d680f3103e2dcc97f9522f75da58
lam.ho <holam9120@gmail.com>
2024-09-07 17:09:32 +0700

    update env

--- src/app/(others)/quizzes/ranking/page.tsx
+++ src/app/(others)/quizzes/ranking/page.tsx
@@ -1,7 +1,7 @@
 import { QuizRanking } from "@/modules/quizzes/ranking";
 
 export default function RankingPage() {
-  if (process.env.NODE_ENV === "production")
+  if (process.env.NEXT_ENV === "production")
     return <div>Tính năng này sẽ ra mắt sớm</div>;
 
   return <QuizRanking />;
