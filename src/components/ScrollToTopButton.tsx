273d5ed497115aa408357f93c188eeebcc29499b
lam.ho <holam9120@gmail.com>
2024-09-08 07:58:17 +0700

    update scrollToTop button

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -10,10 +10,10 @@ export const ScrollToTopButton = () => {
   return (
     <Button
       onClick={scrollToTop}
-      className="fixed bottom-5 right-5 rounded-full p-2"
-      variant="secondary"
+      className="fixed bottom-5 bg-gradient-to-r from-[#8b0000] to-[#cd5c5c] right-5 rounded-full size-12 p-2"
+      variant="ghost"
     >
-      <ChevronUp className="h-7 w-7" />
+      <ChevronUp className="h-7 w-7 text-white" />
     </Button>
   );
 };
