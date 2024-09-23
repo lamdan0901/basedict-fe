9def43d0cb02d4829906396b35b67245137d1b45
duc.nguyen <ducnta.work@gmail.com>
2024-09-23 22:55:00 +0900

    fix validate

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,7 +1,7 @@
 export const MEANING_ERR_MSG = {
   NOT_FOUND: "Không tìm thấy từ",
   VALIDATION_FAILED:
-    "Từ vựng chỉ được bao gồm hán tự, hiragana hoặc toàn bộ là katakana và tối đa là 7 kí tự",
+    "Từ vựng chỉ được bao gồm hán tự, hiragana hoặc katakana và tối đa là 10 kí tự",
   ENDING_WITH_FORBIDDEN: "Không được kết thúc bằng các từ không được phép",
   UNKNOWN: "Đã xảy ra lỗi không xác định",
 };
--- src/lib/japaneseValidators.ts
+++ src/lib/japaneseValidators.ts
@@ -1,35 +1,51 @@
-const MAX_LENGTH = 7;
+const MAX_LENGTH = 10;
 
-// Function to validate if the text contains only Kanji or Hiragana, or only Katakana characters
+// Function to validate if the text contains only Kanji or Hiragan or Katakana characters
 export function isJapanese(text: string): boolean {
   const isLengthValid = text.length <= MAX_LENGTH;
   if (!isLengthValid) return false;
-  return /^(?:[\u4E00-\u9FBF\u3040-\u309F]+|[\u30A0-\u30FF]+)$/.test(text);
+  return /^[\u4E00-\u9FAF\u3040-\u309F\u30A0-\u30FF\u30FC\u3005]+$/.test(
+    text,
+  );
 }
 
 // Function to validate if the text does not end with forbidden forms
 export function isNotEndingWithForbiddenForms(text: string): boolean {
-  const acceptWords = ["ごった", "ばった", "めった"];
+  const acceptWords = [
+    'ごった',
+    'ばった',
+    'めった',
+    'きって',
+    'されば',
+    'ぬれば',
+  ];
   if (acceptWords.includes(text)) return true;
 
+  const forbiddenInclude = ['を', '　'];
+
   const forbiddenEndings = [
-    "ました",
-    "います",
-    "きます",
-    "ります",
-    "します",
-    "させる",
-    "られる",
-    "せます",
-    "れます",
-    "です",
-    "った",
-    "でした",
-    "させた",
-    "られた",
+    'ました',
+    'います',
+    'きます',
+    'ります',
+    'します',
+    'させる',
+    'られる',
+    'せます',
+    'れます',
+    'です',
+    'った',
+    'って',
+    'でした',
+    'させた',
+    'られた',
+    'れば',
   ];
 
-  return !forbiddenEndings.some((ending) => text.endsWith(ending));
+  return (
+    !forbiddenInclude.some((include) => text.includes(include)) &&
+    !forbiddenEndings.some((ending) => text.endsWith(ending))
+  );
 }
 
 // Example usage
