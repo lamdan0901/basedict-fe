f058cb29fc46a3f12041bcf213e6ed89892b7612
lam.ho <holam9120@gmail.com>
2024-10-18 10:22:55 +0700

    try to fix Content-Security-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,24 @@ const nextConfig = {
             },
           ],
         },
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: `
+                default-src 'self';
+                script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://tpc.googlesyndication.com https://www.googletagservices.com;
+                style-src 'self' 'unsafe-inline';
+                img-src 'self' data: https://pagead2.googlesyndication.com;
+                frame-src 'self' https://googleads.g.doubleclick.net https://tpc.googlesyndication.com https://www.google.com;
+                connect-src 'self' https://ep1.adtrafficquality.google https://backend-new-auth-fuw746zcgq-an.a.run.app https://pagead2.googlesyndication.com;
+              `
+                .replace(/\s{2,}/g, " ")
+                .trim(),
+            },
+          ],
+        },
       ];
     }
     return [];
